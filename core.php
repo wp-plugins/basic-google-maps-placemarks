e8aa8b2bb495b8445e8c78f7dc87183b0165cb5b
iandunn <iandunn@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-11 02:11:14 +0000

    Updating Chinese mo/po, minor readme/TODO updates, ready for 1.8
    git-svn-id: https://plugins.svn.wordpress.org/basic-google-maps-placemarks/trunk@584123 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- core.php
+++ core.php
@@ -15,7 +15,7 @@ if( !class_exists( 'BasicGoogleMapsPlacemarks' ) )
 	{
 		// Declare variables and constants
 		protected $settings, $options, $updatedOptions, $userMessageCount, $mapShortcodeCalled, $mapShortcodeArguments, $mapShortcodeCategories;
-		const VERSION		= '1.8-rc1';
+		const VERSION		= '1.8';
 		const PREFIX		= 'bgmp_';
 		const POST_TYPE		= 'bgmp';
 		const TAXONOMY		= 'bgmp-category';
