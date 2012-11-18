c886ee840b3ebf9ecda8bdff359fa3b46df8ed21
iandunn <iandunn@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-18 18:54:48 +0000

    Updating copyright, TODO
    git-svn-id: https://plugins.svn.wordpress.org/basic-google-maps-placemarks/trunk@626677 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- basic-google-maps-placemarks.php
+++ basic-google-maps-placemarks.php
@@ -12,7 +12,7 @@ License: GPL2
 */
 
 /*  
- * Copyright 2011 Ian Dunn (email : ian@iandunn.name)
+ * Copyright 2011-2012 Ian Dunn (email : ian@iandunn.name)
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 2, as 
