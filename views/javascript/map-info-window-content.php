3e3b6d308ba695d8d880e8270c415de75a08df73
iandunn <iandunn@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-03 01:36:07 +0000

    Add template for info window content.
    This was accidentally left out of an earlier commit.
    
    
    git-svn-id: https://plugins.svn.wordpress.org/basic-google-maps-placemarks/trunk@1152029 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ views/javascript/map-info-window-content.php
@@ -0,0 +1,9 @@
+<?php // Note: Title and details have already been sanitized by WP, so they can used be safely without escaping ?>
+
+<script id="tmpl-bgmp-info-window-content" type="text/html">
+	<div class="bgmp_placemark">
+		<h3>{{{title}}}</h3>
+
+		<div>{{{details}}}</div>
+	</div>
+</script>
